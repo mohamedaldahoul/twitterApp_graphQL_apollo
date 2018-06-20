1e4fc55eeb70d6476862670136f79388500b3a82
Moaldahoul <ma811forever@gmail.com>
2018-06-20 14:12:13 +0200

    create graphQL server

---
+++ server.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import {graphqlExpress, graphiqlExpress} from 'apollo-server-express';
+import bodyParser from 'body-parser';
+
+const server = express();
+
+server.use('/graphiql',
+    graphiqlExpress({
+        endpointURL: "/graphql"
+    })
+);
+
+server.use('/graphql', bodyParser.json(), graphqlExpress({}));
+
+server.listen(4000, ()=>{
+    console.log('Listening on post 4000');
+    
+});
